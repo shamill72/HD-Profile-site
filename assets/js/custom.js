8a05bbe3b0b361d6e911072ac584924a4439a983
shamill72 <75394386+shamill72@users.noreply.github.com>
2024-10-06 14:53:32 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
                 user_id: userId
             },
             success: function(response) {
-                console.log(response.liked);
+                console.log(response);
                 if(response.liked) {
                     console.log("It was liked");
                     fetchLikes();
